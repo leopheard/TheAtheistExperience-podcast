d1c6d204ae07008e5766fa786bca9a6e50135de5
leopheard <leopheard.misc@gmail.com>
2019-07-14 16:01:20 -0400

    add archive section

--- addon.py
+++ addon.py
@@ -4,6 +4,7 @@ from resources.lib import theatheistexperiencepodcast
 plugin = Plugin()
 
 URL = "https://www.spreaker.com/show/3254896/episodes/feed"
+URL2 = "http://atheist-experience.com/archive/?full=1#table"
 
 @plugin.route('/')
 def main_menu():
@@ -18,6 +19,10 @@ def main_menu():
         {
             'label': plugin.get_string(30000), 
             'path': plugin.url_for('all_episodes1'),
+            'thumbnail': "https://d3wo5wojvuv7l.cloudfront.net/t_rss_itunes_square_1400/images.spreaker.com/original/759ffa12e832b60f7779066e7b874dba.jpg"},
+        {
+            'label': plugin.get_string(30002), 
+            'path': plugin.url_for('all_episodes2'),
             'thumbnail': "https://d3wo5wojvuv7l.cloudfront.net/t_rss_itunes_square_1400/images.spreaker.com/original/759ffa12e832b60f7779066e7b874dba.jpg"},
     ]
 
@@ -48,6 +53,19 @@ def all_episodes1():
     playable_podcast1 = theatheistexperiencepodcast.get_playable_podcast1(soup)
     
     items = theatheistexperiencepodcast.compile_playable_podcast1(playable_podcast1)
+
+    return items
+
+@plugin.route('/all_episodes2/')
+def all_episodes1():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup2 = theatheistexperiencepodcast.get_soup(URL2)
+    
+    playable_podcast2 = theatheistexperiencepodcast.get_playable_podcast1(soup2)
+    
+    items = theatheistexperiencepodcast.compile_playable_podcast2(playable_podcast2)
 
     return items
 
--- resources/lib/theatheistexperiencepodcast.py
+++ resources/lib/theatheistexperiencepodcast.py
@@ -14,6 +14,18 @@ def get_soup(url):
 
 get_soup("https://www.spreaker.com/show/3254896/episodes/feed")
 
+def get_soup2(url2):
+    """
+    @param: url of site to be scraped
+    """
+    page = requests.get(url2)
+    soup2 = BeautifulSoup(page.text, 'html.parser')
+    
+    print "type: ", type(soup2)
+    return soup2
+
+get_soup("https://view-source:http://atheist-experience.com/archive/?full=1#table")
+
 
 def get_playable_podcast(soup):
     """
@@ -114,6 +126,59 @@ def compile_playable_podcast1(playable_podcast1):
     items = []
 
     for podcast in playable_podcast1:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+#            'info': podcast['desc'],
+            'is_playable': True,
+    })
+
+    return items
+
+
+def get_playable_podcast2(soup2):
+    """
+    @param: parsed html page            
+    """
+    subjects = []
+
+    for content in soup2.find_all('tr'):
+        
+        try:        
+            link = content.find('a type="audio/mpeg')
+            link = link.get('href')
+            print "\n\nLink: ", link
+
+            title = content.find('td','td valign=top')
+            title = title.get_text()
+
+#            desc = content.find('itunes:subtitle')
+#            desc = desc.get_text()
+
+        except AttributeError:
+            continue
+              
+        item = {
+                'url': link,
+                'title': title,
+#                'desc': desc,
+                'thumbnail': "https://d3wo5wojvuv7l.cloudfront.net/t_rss_itunes_square_1400/images.spreaker.com/original/759ffa12e832b60f7779066e7b874dba.jpg"
+        }
+        
+        subjects.append(item) 
+    
+    return subjects
+
+
+def compile_playable_podcast2(playable_podcast2):
+    """
+    @para: list containing dict of key/values pairs for playable podcasts
+
+    """
+    items = []
+
+    for podcast in playable_podcast2:
         items.append({
             'label': podcast['title'],
             'thumbnail': podcast['thumbnail'],
