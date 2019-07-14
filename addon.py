1cf3b7f333925b4686d4ae06469f0d2a7e1c3515
leopheard <leopheard.misc@gmail.com>
2019-07-14 15:22:33 -0400

    initial

---
+++ addon.py
@@ -0,0 +1,56 @@
+from xbmcswift2 import Plugin, xbmcgui
+from resources.lib import theatheistexperience
+
+plugin = Plugin()
+
+URL = "https://www.spreaker.com/show/3254896/episodes/feed"
+
+@plugin.route('/')
+def main_menu():
+    """
+    main menu 
+    """
+    items = [
+        {
+            'label': plugin.get_string(30001), 
+            'path': plugin.url_for('all_episodes'),
+            'thumbnail': "https://d3wo5wojvuv7l.cloudfront.net/t_rss_itunes_square_1400/images.spreaker.com/original/759ffa12e832b60f7779066e7b874dba.jpg"},
+        {
+            'label': plugin.get_string(30000), 
+            'path': plugin.url_for('all_episodes1'),
+            'thumbnail': "https://d3wo5wojvuv7l.cloudfront.net/t_rss_itunes_square_1400/images.spreaker.com/original/759ffa12e832b60f7779066e7b874dba.jpg"},
+    ]
+
+    return items
+
+
+@plugin.route('/all_episodes/')
+def all_episodes():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup = theatheistexperience.get_soup(URL)
+    
+    playable_podcast = theatheistexperience.get_playable_podcast(soup)
+    
+    items = theatheistexperience.compile_playable_podcast(playable_podcast)
+
+    return items
+
+
+@plugin.route('/all_episodes1/')
+def all_episodes1():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup = theatheistexperience.get_soup(URL)
+    
+    playable_podcast1 = theatheistexperience.get_playable_podcast1(soup)
+    
+    items = theatheistexperience.compile_playable_podcast1(playable_podcast1)
+
+    return items
+
+
+if __name__ == '__main__':
+    plugin.run()
